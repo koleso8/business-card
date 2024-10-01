1a10dd2e6d465bedc9cc82ffd6f6bd62b7dbc4b6
koleso8 <dev.koleso@gmail.com>
2024-10-01 10:47:04 +0300

    add theme button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,13 @@ import { RxColorWheel } from 'react-icons/rx';
 import { startEffect } from '../../util/vantaAnimation';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeTheme, setTheme } from '../../redux/theme/slice';
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import { selectTheme } from '../../redux/theme/selectors';
+import { MdSunny } from 'react-icons/md';
 
 const Header = () => {
   const dispatch = useDispatch();
-
+  const theme = useSelector(selectTheme);
   useEffect(() => {
     headerHide();
     startEffect();
@@ -15,12 +18,18 @@ const Header = () => {
   });
   return (
     <header className="fixed backdrop-blur-3xl w-full transition-transform ease-in-out delay-150 duration-300 header flex items-center ">
-      <RxColorWheel
-        size="60px"
-        className="animate-spin-slow text-[--color-text]"
-      />
-      <h2 className=" text-4xl text-[--color-text]">dev.Koleso</h2>
-      <button onClick={() => dispatch(changeTheme())}>☼</button>
+      <RxColorWheel size="60px" className="animate-spin-slow text-[--color]" />
+      <h2 className=" text-4xl text-[--color]">dev.Koleso</h2>
+      <button
+        onClick={() => dispatch(changeTheme())}
+        className="flex justify-center items-center bg-[--color]  w-10 h-10 p-1 rounded-full"
+      >
+        {theme === 'light' ? (
+          <BsFillMoonStarsFill size="26px" className="text-[--bg-color]" />
+        ) : (
+          <MdSunny size="30px" className="text-[--bg-color]" />
+        )}
+      </button>
     </header>
   );
 };
