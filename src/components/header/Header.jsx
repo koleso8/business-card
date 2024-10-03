7b7e6e2dec3bd3e3c672731757b99f72a19fe92d
koleso8 <dev.koleso@gmail.com>
2024-10-03 20:13:38 +0300

    fix background animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -17,19 +17,24 @@ const Header = () => {
     dispatch(setTheme());
   });
   return (
-    <header className="fixed backdrop-blur-3xl w-full transition-transform ease-in-out delay-150 duration-300 header flex items-center ">
-      <RxColorWheel size="60px" className="animate-spin-slow text-[--color]" />
-      <h2 className=" text-4xl text-[--color]">dev.Koleso</h2>
-      <button
-        onClick={() => dispatch(changeTheme())}
-        className="flex justify-center items-center bg-[--color]  w-10 h-10 p-1 rounded-full"
-      >
-        {theme === 'light' ? (
-          <BsFillMoonStarsFill size="26px" className="text-[--bg-color]" />
-        ) : (
-          <MdSunny size="30px" className="text-[--bg-color]" />
-        )}
-      </button>
+    <header className="fixed backdrop-blur-3xl w-screen transition-transform ease-in-out delay-150 duration-300 header flex items-center justify-center  py-2">
+      <div className="w-[90%] flex items-center gap-3">
+        <RxColorWheel
+          size="60px"
+          className="animate-spin-slow text-[--color]"
+        />
+        <h2 className=" text-4xl text-[--color]">dev.Koleso</h2>
+        <button
+          onClick={() => dispatch(changeTheme())}
+          className="flex justify-center items-center bg-[--color]  w-10 h-10 p-1 rounded-full"
+        >
+          {theme === 'light' ? (
+            <BsFillMoonStarsFill size="26px" className="text-[--bg-color]" />
+          ) : (
+            <MdSunny size="30px" className="text-[--bg-color]" />
+          )}
+        </button>
+      </div>
     </header>
   );
 };
